852aeb12342089085370e7f103ce987e7eb71825
admin <d.art.shishkin@gmail.com>
2020-06-15 23:34:30 +0300

    Assignment - JUnit 5 Yanny Test

--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterYannyTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterYannyTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.sfg.BaseConfig;
 import org.springframework.samples.petclinic.sfg.HearingInterpreter;
-import org.springframework.samples.petclinic.sfg.LaurelConfig;
+import org.springframework.samples.petclinic.sfg.YannyConfig;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfig.class})
-class HearingInterpreterTest {
+@SpringJUnitConfig(classes = {BaseConfig.class, YannyConfig.class})
+class HearingInterpreterYannyTest {
 
     @Autowired
     HearingInterpreter hearingInterpreter;
@@ -18,6 +18,6 @@ class HearingInterpreterTest {
     @Test
     void whatIheard() {
         String word = hearingInterpreter.whatIheard();
-        assertEquals("Laurel",word);
+        assertEquals("Yanny",word);
     }
 }
